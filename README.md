160afb41987fcb288d3d331153cd9eb0f49ccd98
arbitar <github@arbi.in>
2022-12-15 21:18:42 -0800

    Add flags, env, & multi-file handling.

--- README.md
+++ README.md
@@ -1,17 +1,139 @@
 # uhandles
+("micro-handles": for small, portable handlebars)
 
 An escalation of [jsonsubst](https://github.com/arbitar/jsonsubst).  
-Small static Go program with no external runtime dependencies that takes a [handlebars](https://handlebarsjs.com) template in from stdin, renders it with data from a JSON file (passed by argument), and outputs the result to stdout.  
-This is basically just a simple wrapper around [aymerick/raymond](https://github.com/aymerick/raymond).
+Small static Go program with no external runtime dependencies that takes [handlebars](https://handlebarsjs.com) template(s), renders it with reference to JSON data, and outputs the results as directed.  
+This is basically just a wrapper around [aymerick/raymond](https://github.com/aymerick/raymond).
 
-## Usage
-Very simple. One argument: JSON file.
+## Basic Usage
+In its simplest form, one argument:
 
 `./uhandles data.json <input.txt >output.txt`
 
 Or, in a pipeline of some kind...
 
 `cat input.txt | ./uhandles data.json | tee output.txt`
+
+**Example:**  
+*data.json*:
+```
+{
+	"object": {
+		"property": "value",
+		"list": ["one", "two", "three"]
+	},
+	"objects": [
+		{ "lower": "a", "upper": "A" },
+		{ "lower": "b", "upper": "B" }
+	]
+}
+```
+*template.tmpl.txt*:
+```
+Hi! This is object.property's value: {{ object.property }}
+
+Here's what's in an array:
+{{#each object.list}}
+- {{this}}
+{{/each}}
+
+Here's what's in an array of objects:
+{{#each objects}}
+{{#with this}}
+- The upper-case version of "{{lower}}" is "{{upper}}"
+{{/with}}
+{{/each}}
+```
+
+After running:  
+`./uhandles data.json <template.tmpl.txt`
+
+The expected output on stdout would be:
+```
+Hi! This is object.property's value: value
+
+Here's what's in an array:
+- one
+- two
+- three
+
+Here's what's in an array of objects:
+- The upper-case version of "a" is "A"
+- The upper-case version of "b" is "B"
+```
+
+## Advanced Usage
+If you don't want to the template coming from stdin, or the output to stdout, or if you want the template from a file and the JSON from stdin, or any other number of weird things... there is a more complicated flag-based usage:
+
+`./uhandles --data data.json --template input.txt --output output.txt`
+
+You can use `-` for any of these file paths to specify stdin/stdout as appropriate. It will complain if you specify `-` for both data and template, so don't get too clever.
+
+| Flag                            | Description |
+| ------------------------------- | - |
+| `-d\|--data <file\|->`          | JSON file path to use to render the template, or "-" for stdin |
+| `-t\|--template <file\|dir\|->` | Input Handlebars template path, or directory, or "-" for stdin |
+| `-o\|--output <file\|dir\|->`   | Output file path, or directory, or "-" for stdout |
+| `--tmpl-token <token>`          | Token to use to detect template files. Default: `".tmpl"`. Is assembled into a glob like `*<token>*`, and output files have instances of `<token>` removed.
+
+## Environment Variables
+uhandles will look for the following environment variables and use them in lieu of their associated flags:
+
+| Env                   | Flag               |
+| --------------------- | ------------------ |
+| `UHANDLES_DATA`       | `-d`, `--data`     |
+| `UHANDLES_TEMPLATE`   | `-t`, `--template` |
+| `UHANDLES_OUTPUT`     | `-o`, `--output`   |
+| `UHANDLES_TMPL_TOKEN` | `--tmpl-token`     |
+
+## Directory Of Templates Rendering
+When a directory is specified for `--template`, it looks within for any `*.tmpl*` files within it. Each of these files is interpreted as a template and separately rendered with the data from the JSON file. If `--output` specifies a directory, a corresponding file to each template (with the same permissions and the tmpl-token removed from the filename) will be created in the output directory. Otherwise, if `--output` specifies a single file or stdout, concatenation will be performed as per the next section.
+
+**Example:**  
+Input directory:  
+```
+./input/
+  input_1.tmpl.txt
+	input_2.txt
+	input_3.txt.tmpl
+```
+
+With an output directory empty, but present, at `./output/`, then run with:  
+`./uhandles -d data.json -t ./input/ -o ./output/`
+
+This results in an output directory containing the rendered templates:  
+```
+./output/
+  input_1.txt
+  input_3.txt
+```
+
+Note that `input_2.txt` is ignored, because it does not contain the template token.
+
+If the output directory already contains files, they will be left in place, unless they are to be written to by the current operation. If so, they are overwritten entirely and their mode is changed to match the input files.
+
+## Concatenation
+When there are multiple template files but only one output file (a file path or stdout), the multiple template files are rendered in directory listing order and concatenated together with single newlines between them into the single output file.
+
+**Example:**  
+Input directory:
+```
+./input/
+  input_1.tmpl.txt
+  input_2.txt
+  input_3.txt.tmpl
+```
+
+Run with:  
+`./uhandles -d data.json -t ./input/ -o ./output.txt`
+
+Results in a single file, output.txt, containing:
+```
+<rendered outputs of input_1.tmpl.txt>
+<rendered outputs of input_3.txt.tmpl>
+```
+
+Note the newline between the two. Also, `input_2.txt` is ignored, because it does not contain the template token. Output could also have been omitted or `-` to direct the output to stdout.
 
 ## Build
 If you're cool and already have a good working Go build environment on your system, just run `make build`.
--- uhandles.go
+++ uhandles.go
@@ -6,42 +6,260 @@ import (
 	"io/ioutil"
 	"os"
 	"strings"
-            
+	"flag"
+	"path"
+	"path/filepath"
+
 	"github.com/aymerick/raymond"
 )
 
+func usage(dest *os.File) {
+	fmt.Fprintln(dest, "Usage:")
+	fmt.Fprintln(dest, "  [...] | " + os.Args[0] + " <json-data-file> | [...]")
+	fmt.Fprintln(dest, "  " + os.Args[0] + " [-d|--data <json-data-file|->] [-t|--template <tmpl-file|tmpl-dir|->] [-o|--output <output-file|output-dir|->]")
+	fmt.Fprintln(dest, `
+  -d|--data 		JSON data file containing substitution values (file path or "-" for stdin)
+  -t|--template 	Handlebar template file to render, or directory of template files (default: "-", stdin)
+  -o|--output 		File to concat rendered templates to, or directory to place rendered templates within (default: "-", stdout)
+	--tmpl-token 		Token to use to detect template files in input-directory mode (default: ".tmpl")
+
+  (NOTE: You cannot specify "-"/stdin as a source for both template and data.)
+`)
+}
+
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Println("Usage: uhandlebars <json file>")
-		os.Exit(1)
+	// quick short-circuit to usage info:
+	if len(os.Args) > 2 && (os.Args[1] == "-h" || os.Args[1] == "--help") {
+		usage(os.Stdout)
+		os.Exit(0)
 	}
 
-	jsonFile := os.Args[1]
+	// handle flags:
+	var jsonPath string
+	var inputPath string
+	var outputPath string
+	var tmplToken string
 
-	jsonData, err := ioutil.ReadFile(jsonFile)
-	if err != nil {
-		fmt.Println("Error reading JSON file:", err)
-		os.Exit(1)
+	flag.StringVar(&jsonPath, "d", "", "")
+	flag.StringVar(&jsonPath, "data", "", "")
+
+	flag.StringVar(&inputPath, "t", "", "")
+	flag.StringVar(&inputPath, "template", "", "Handl")
+
+	flag.StringVar(&outputPath, "o", "", "")
+	flag.StringVar(&outputPath, "output", "", "")
+
+	flag.StringVar(&tmplToken, "tmpl-token", "", "")
+
+	flag.Parse()
+
+	// pluck defaults out of env variables if they're set but not specified from flags:
+	// os.Getenv defaults to blank strings if the value isn't present, conveniently
+	if jsonPath == "" {
+		jsonPath = os.Getenv("UHANDLES_DATA")
 	}
 
+	if inputPath == "" {
+		inputPath = os.Getenv("UHANDLES_TEMPLATE")
+	}
+
+	if outputPath == "" {
+		outputPath = os.Getenv("UHANDLES_OUTPUT")
+	}
+
+	if tmplToken == "" {
+		tmplToken = os.Getenv("UHANDLES_TMPL_TOKEN")
+	}
+
+	// if we're still unset, set to our default...
+	if tmplToken == "" {
+		tmplToken = ".tmpl"
+	}
+
+	// if we still have no paths from env and no other arguments... show usage politely:
+	if len(os.Args) == 1 && jsonPath == "" && inputPath == "" && outputPath == "" {
+		usage(os.Stdout)
+		os.Exit(0)
+	}
+
+	// pull JSON data from argv[1] if not specified with a flag above:
+	if jsonPath == "" && jsonPath != "-" {
+		if len(os.Args) != 2 {
+			fmt.Println("No data file specified with -d|--data and no data file found in first argument.")
+			usage(os.Stderr)
+			os.Exit(1)
+		}
+
+		jsonPath = os.Args[1]
+	}
+
+	// read JSON data from desired source:
+	var jsonData []byte
+
+	if jsonPath != "-" {
+		// read from file
+		readData, err := ioutil.ReadFile(jsonPath)
+		if err != nil {
+			fmt.Println("Error reading JSON file:", err)
+			usage(os.Stderr)
+			os.Exit(1)
+		}
+		jsonData = readData
+	} else {
+		// read from stdin
+		readData, err := ioutil.ReadAll(os.Stdin)
+		if err != nil {
+			fmt.Println("Error reading JSON from stdin:", err)
+			usage(os.Stderr)
+			os.Exit(1)
+		}
+		jsonData = readData
+	}
+
+	// parse read JSON data
 	var data interface{}
-	err = json.Unmarshal(jsonData, &data)
+	err := json.Unmarshal(jsonData, &data)
 	if err != nil {
 		fmt.Println("Error parsing JSON file:", err)
 		os.Exit(1)
 	}
 
-	template, err := ioutil.ReadAll(os.Stdin)
-	if err != nil {
-		fmt.Println("Error reading template from stdin:", err)
-		os.Exit(1)
+	// pull out real input and output paths depending on if the path supplied is a directory or not:
+	var inputPaths []string
+	var outputPaths []string
+	var outputPathIsDir bool
+
+	if inputPath == "" || inputPath == "-" {
+		// input is either implicitly or explicitly stdin
+		inputPaths = []string{"-"}
+	} else {
+		// sample input path to see if it's a directory
+		inputPathInfo, err := os.Stat(inputPath)
+		if err != nil {
+			fmt.Println("Cannot stat input path:", inputPath, err)
+			usage(os.Stderr)
+			os.Exit(1)
+		}
+	
+		if inputPathInfo.IsDir() {
+			// populate input paths by globbing for template files in the input dir
+			inputPaths, err = filepath.Glob(path.Clean(inputPath) + "/*" + tmplToken + "*")
+			if err != nil {
+				fmt.Println("Cannot glob input path:", inputPath, err)
+				usage(os.Stderr)
+				os.Exit(1)
+			}
+		} else {
+			// we're just a file
+			inputPaths = []string{path.Clean(inputPath)}
+		}
 	}
 
-	result, err := raymond.Render(string(template), data)
-	if err != nil {
-		fmt.Println("Error rendering template:", err)
-		os.Exit(1)
+	if outputPath == "" || outputPath == "-" {
+		// output is either implicitly or explicitly stdin
+		// ... just alter the output path to stdin simplify it for us later
+		outputPath = "-"
+	} else {
+		// sample output path to see if it's a directory
+		outputPathInfo, err := os.Stat(outputPath)
+		if err != nil {
+			fmt.Println("Cannot stat output path:", outputPath, err)
+			usage(os.Stderr)
+			os.Exit(1)
+		}
+
+		// retain for later append-vs-write logic
+		outputPathIsDir = outputPathInfo.IsDir()
+
+		if outputPathIsDir {
+			// if input is a single file or a directory of files, strip the .tmpl from their names and
+			// place them within the destination directory
+			for i := 0; i < len(inputPaths); i++ {
+				baseName := strings.Replace(path.Base(inputPaths[i]), tmplToken, "", -1)
+				newName := path.Join(outputPath, baseName)
+				outputPaths = append(outputPaths, newName)
+			}
+		}
 	}
 
-	fmt.Println(strings.TrimSpace(result))
+	// iterate through all the inputs as directed, render them with the data, and output them as prescribed
+	var outputHandle *os.File // needed for appending multiple inputs to a single file
+	for idx, inPath := range inputPaths {
+		// read template...
+		var template []byte
+
+		if inPath != "-" {
+			// read template file
+			reader, err := os.Open(inPath)
+			if err != nil {
+				fmt.Println("Error opening template file:", inPath, err)
+				usage(os.Stderr)
+				os.Exit(1)
+			}
+
+			template, err = ioutil.ReadAll(reader)
+			if err != nil {
+				fmt.Println("Error reading template file:", inPath, err)
+				usage(os.Stderr)
+				os.Exit(1)
+			}
+			reader.Close()
+		} else {
+			// read template from stdin
+			if jsonPath == "-" {
+				fmt.Println("Can't read template from stdin, already reading JSON from stdin.")
+				usage(os.Stderr)
+				os.Exit(1)
+			}
+
+			readTemplate, err := ioutil.ReadAll(os.Stdin)
+			if err != nil {
+				fmt.Println("Error reading template from stdin:", err)
+				usage(os.Stderr)
+				os.Exit(1)
+			}
+
+			template = readTemplate
+		}
+
+		// render template from input with data
+		result, err := raymond.Render(string(template), data)
+		if err != nil {
+			fmt.Println("Error rendering template:", err)
+			usage(os.Stderr)
+			os.Exit(1)
+		}
+		
+		// output the rendering
+		if outputPath == "" || outputPath == "-" {
+			// outputing to stdout, just dump it out. it'll automatically "append" if there are more inputs
+			fmt.Println(strings.TrimSpace(result))
+		} else if outputPathIsDir {
+			// snag corresponding input file permissions
+			inStat, _ := os.Stat(inPath)
+
+			// outputting to separate file(s) in a dir with the input file permissions
+			ioutil.WriteFile(outputPaths[idx], []byte(strings.TrimSpace(result)), inStat.Mode())
+		} else {
+			// outputting to a single file, appending renders
+			if idx == 0 {
+				// snag (first) input file permissions
+				inStat, _ := os.Stat(inPath)
+				
+				// if this is the first rendering, create the file with the input permissions
+				ioutil.WriteFile(outputPath, []byte(strings.TrimSpace(result)), inStat.Mode())
+
+				// and open it for further append writing...
+				outputHandle, _ = os.OpenFile(outputPath, os.O_APPEND|os.O_WRONLY, inStat.Mode())
+			} else {
+				// if this isn't the first rendering, append to the file
+				outputHandle.WriteString("\n" + strings.TrimSpace(result))
+			}
+		}
+	}
+
+	// close single-output appending output handle, if it was used
+	if outputHandle != nil {
+		outputHandle.Close()
+	}
 }
